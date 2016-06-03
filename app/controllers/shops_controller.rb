2f3e0e976e9493442631e222e732e428c0d6f62e
zhangkun <313822030@qq.com>
2016-06-03 12:40:07 +0800

    test product init

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -52,7 +52,7 @@ class ShopsController < ApplicationController
 
   def init_categories_products
     authorize @shop
-    Category.init_shop_categories(@shop)
+    # Category.init_shop_categories(@shop)
     Product.init_shop_products(@shop)
     flash[:success] = '店铺初始化分类、产品成功！'
     render js: "location.reload();"
