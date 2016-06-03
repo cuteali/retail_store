a978122099d5d5e9935597324b21a031cc7745f3
zhangkun <313822030@qq.com>
2016-06-03 13:03:26 +0800

    fixed init

--- app/models/product.rb
+++ app/models/product.rb
@@ -21,15 +21,17 @@ class Product < ActiveRecord::Base
   def self.init_shop_products(shop)
     Product.normal.sorted.each do |p|
       category = shop.categories.normal.find_by(name: p.category.try(:name))
-      sub_category = shop.sub_categories.normal.find_by(name: p.sub_category.try(:name))
-      detail_category = shop.detail_categories.normal.find_by(name: p.detail_category.try(:name))
-      shop_product = shop.shop_products.create(product_id: p.id, category_id: category.id, sub_category_id: sub_category.id, detail_category_id: detail_category.id, unit_id: p.unit.id,
-        name: p.name, price: p.price, old_price: p.old_price, stock_volume: p.stock_volume, sales_volume: p.sales_volume, desc: p.desc, info: p.info, spec: p.spec, sort: p.sort,
-        is_app_index: p.is_app_index, state: p.state)
-      shop_product.update_columns(key: p.key.path)
-      p.images.normal.each do |image|
-        shop_product_image = shop_product.images.create
-        shop_product_image.update_columns(key: image.key.path)
+      sub_category = category.sub_categories.normal.find_by(name: p.sub_category.try(:name))
+      detail_category = sub_category.detail_categories.normal.find_by(name: p.detail_category.try(:name))
+      if category && sub_category && detail_category
+        shop_product = shop.shop_products.create(product_id: p.id, category_id: category.id, sub_category_id: sub_category.id, detail_category_id: detail_category.id, unit_id: p.unit.id,
+          name: p.name, price: p.price, old_price: p.old_price, stock_volume: p.stock_volume, sales_volume: p.sales_volume, desc: p.desc, info: p.info, spec: p.spec, sort: p.sort,
+          is_app_index: p.is_app_index, state: p.state)
+        shop_product.update_columns(key: p.key.path)
+        p.images.normal.each do |image|
+          shop_product_image = shop_product.images.create
+          shop_product_image.update_columns(key: image.key.path)
+        end
       end
     end
   end
