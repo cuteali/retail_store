1b9660982e3bed21d7b4ef67da947a4693963eb4
zhangkun <ay_zhangkun@sina.com>
2016-06-06 10:46:22 +0000

    fixed qiniu

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -24,4 +24,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
     var = :"@#{mounted_as}_timestamp"
     model.instance_variable_get(var) or model.instance_variable_set(var, Time.now.to_i)
   end
+
+  def delete
+    nil
+  end
+
 end
